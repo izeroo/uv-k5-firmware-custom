ea3eefafdd7becf42ca7656bbfc5f16bd6c84901
wu58430 <wu58430@126.com>
2024-06-08 15:01:16 +0800

    全编译

--- gen.py
+++ gen.py
@@ -1,22 +1,21 @@
 import os
 import time
-from concurrent.futures import ThreadPoolExecutor
 
-def alter(file, old_str, new_str):
+def alter(file,old_str,new_str):
     """
     替换文件中的字符串
-    :param file: 文件名
-    :param old_str: 旧字符串
-    :param new_str: 新字符串
+    :param file:文件名
+    :param old_str:就字符串
+    :param new_str:新字符串
     :return:
     """
     file_data = ""
     with open(file, "r", encoding="utf-8") as f:
         for line in f:
             if old_str in line:
-                line = line.replace(old_str, new_str)
+                line = line.replace(old_str,new_str)
             file_data += line
-    with open(file, "w", encoding="utf-8") as f:
+    with open(file,"w",encoding="utf-8") as f:
         f.write(file_data)
 
 list1 = [
@@ -49,6 +48,7 @@ list6 = [
     'ENABLE_FMRADIO=1'
 ]
 
+
 list7 = [
     'ENABLE_PINYIN=1',
 ]
@@ -57,8 +57,14 @@ list8 = [
     'ENABLE_SPECTRUM=1'
 ]
 
-list9 = [
-    'ENABLE_4732SSB=1'
+listn = [
+    list1,
+    list2,
+    list3,
+    list4,
+    list5,
+    list6,
+    list7
 ]
 
 strx = []
@@ -69,63 +75,50 @@ for chinese in list1:
         for doppler in [[], list4]:
             for mdc1200 in [[], list5]:
                 for fm in [[], list6[0], list6[1]]:
-                    for pinyin in [[], list7]:
-                        for spectrum in [[], list8]:
-                            for ssb in [[], list9]:
+                    for pinyin in [[],list7]:
+                        for spectrum in [[],list8]:
+                            strm = '';
+                            strn = '';
+                            strm += chinese + ' '
+                            strn += chinese[-1]
+                            if messenger != []:
+                                strm += " ".join(list3) + ' '
+                                strn += '1'
+                            else:
+                                strn += '0'
+                            if doppler != []:
+                                strm += " ".join(list4) + ' '
+                                strn += '1'
+                            else:
+                                strn += '0'
+                            if mdc1200 != []:
+                                strm += " ".join(list5) + ' '
+                                strn += '1'
+                            else:
+                                strn += '0'
+                            if fm != []:
+                                strm += fm + ' '
+                                strn += fm[7]
+                            else:
+                                strn += '0'
+                            if pinyin != []:
+                                strm += " ".join(list7) + ' '
+                                strn += '1'
+                            else:
+                                strn += '0'
+                            if spectrum != []:
+                                strm += " ".join(list8) + ' '
+                                strn += '1'
+                            else:
+                                strn += '0'
+                            strx.append(strm)
+                            stry.append(strn)
 
-                                strm = ''
-                                strn = ''
-                                strm += chinese + ' '
-                                strn += chinese[-1]
-                                if messenger != []:
-                                    strm += " ".join(list3) + ' '
-                                    strn += '1'
-                                else:
-                                    strn += '0'
-                                if doppler != []:
-                                    strm += " ".join(list4) + ' '
-                                    strn += '1'
-                                else:
-                                    strn += '0'
-                                if mdc1200 != []:
-                                    strm += " ".join(list5) + ' '
-                                    strn += '1'
-                                else:
-                                    strn += '0'
-                                if fm != []:
-                                    strm += fm + ' '
-                                    strn += fm[7]
-                                else:
-                                    strn += '0'
-                                if pinyin != []:
-                                    strm += " ".join(list7) + ' '
-                                    strn += '1'
-                                else:
-                                    strn += '0'
-                                if spectrum != []:
-                                    strm += " ".join(list8) + ' '
-                                    strn += '1'
-                                else:
-                                    strn += '0'
-                                if ssb != []:
-                                    strm += " ".join(list9) + ' '
-                                    strn += '1'
-                                else:
-                                    strn += '0'
-                                strx.append(strm)
-                                stry.append(strn)
-
-def compile_code(index):
+for index in range(len(set(strx))):
     print(strx[index])
     print(stry[index])
     os.system("cp Makefile.template Makefile")
     alter("Makefile", "CUSCANSHU", strx[index])
-
     alter("Makefile", "CUSTOMNAME", 'LOSEHU' + stry[index])
-    print(strx[index], stry[index])
     time.sleep(1)
-    os.system("make")
-
-with ThreadPoolExecutor(max_workers=50) as executor:
-    for index in range(len(set(strx))):
-        executor.submit(compile_code, index)
+    os.system("make all")
