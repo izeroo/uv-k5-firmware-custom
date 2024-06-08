c42f43a1f663f5f89bdefc506e1ba98ac10a1606
wu58430 <wu58430@126.com>
2024-06-08 17:04:12 +0800

    生成json

--- genJson.py
+++ genJson.py
@@ -3,7 +3,7 @@ import json
 
 list = []
 for filename in os.listdir(r'.'):
-    if  filename[-5] != 'm' and filename[-4] == '.' and  filename[-3] == 'b' and filename[-2] == 'i' and filename[-1] == 'n'  :
+    if len(filename) >= 5 and filename[-5] != 'm' and filename[-4] == '.' and filename[-3] == 'b' and filename[-2] == 'i' and filename[-1] == 'n':
         list.append(filename)
 
 print(json.dumps(list))
